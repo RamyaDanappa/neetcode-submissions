640d31c1bcbd3c9be8bc8202093637394817447e
Ramya Danappa <46824895+RamyaDanappa@users.noreply.github.com>
2026-08-18 17:24:48 -0500

    Add: singleton - submission-0

---
+++ Design Patterns/singleton/submission-0.py
@@ -0,0 +1,17 @@
+class Singleton:
+
+    # In python consider this method as the 'getInstance'
+    _unique_instance=None
+    def __new__(cls):
+        
+        if cls._unique_instance is None:
+            cls._unique_instance=super(Singleton, cls).__new__(cls)
+        return cls._unique_instance
+
+
+    def getValue(self) -> str:
+        return self.value
+
+    def setValue(self, value: str):
+        self.value =value
+
