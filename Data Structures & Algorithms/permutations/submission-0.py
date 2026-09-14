351fb9630483eefc6136b39381f388478552b2f8
Ramya Danappa <46824895+RamyaDanappa@users.noreply.github.com>
2026-09-14 15:48:18 -0500

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res =[]
+        def helper(nums, slate, res,i):
+            if len(nums)==i:
+                res.append(slate[:])
+                return
+            for j in range(i, len(nums)):
+                nums[i],nums[j]=nums[j],nums[i]
+                slate.append(nums[i])
+                helper(nums, slate, res, i+1)
+                slate.pop()
+                nums[i],nums[j]=nums[j],nums[i]
+        helper(nums, [], res, 0)
+        return res
+
