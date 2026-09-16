9193c962895d134366e37e1b8cb4c014efd7f561
Ramya Danappa <46824895+RamyaDanappa@users.noreply.github.com>
2026-09-16 08:32:53 -0500

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def get_neigbor(x,y):
+            temp=[]
+            if x+1<len(grid):
+                temp.append((x+1, y))
+            if y+1 < len(grid[0]):
+                temp.append((x, y+1))
+            if x-1>=0:
+                temp.append((x-1, y))
+            if y-1>=0:
+                temp.append((x, y-1))
+            return temp
+        def bfs(i,j):
+            q=deque()
+            q.append((i,j))
+            grid[i][j]="0"
+            while q:
+                r,c= q.popleft()
+                cur_neighbor = get_neigbor(r,c)
+                for NR,NC in cur_neighbor:
+                    if grid[NR][NC] !="0":
+                        q.append((NR,NC))
+                        grid[NR][NC]="0"
+        count =0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] !="0":
+                    bfs(i,j)
+                    count+=1
+        return count
+
+        
